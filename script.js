4935c081b3503902605759bf49c91c1dc05fd5c9
Angger Bayu Pangestu <kontak@angger.me>
2021-03-13 16:44:41 +0700

    contoh kasus rest pada function

--- script.js
+++ script.js
@@ -26,6 +26,11 @@ const rumahMakan = {
       tutup: 24,
     },
   },
+
+  mieRebus: function (sawi, ...bumbuLain) {
+    console.log(sawi);
+    console.log(...bumbuLain);
+  },
 };
 /* Tidak termasuk skipped elements hanya benar-benar sisa yang terbaca */
 // ----- Perilaku Rest -----
@@ -37,3 +42,33 @@ console.log(soto, rendang, makananLain); // soto, rendang, [sate, nasi goreng]
 /* Penerapan pada object */
 const { jum, ...akhirPekan } = rumahMakan.jamBuka;
 console.log(akhirPekan); // kam, sab
+
+// ----- REST PARAMETERS PADA FUNCTION -----
+/*Tanpa (...) */
+const angka = function (...a) {
+  console.log(...a);
+};
+/* */
+const t = [8, 3, 4];
+angka(...t);
+angka(1, 2);
+angka(1, 2, 3);
+angka(1, 2, 3, 4);
+
+/* Penerapan lebih dalam  */
+const angka1 = function (...jmlAngka) {
+  let jml = 0;
+  for (let i = 0; i < jmlAngka.length; i++) jml += jmlAngka[i];
+  console.log(jml);
+};
+
+angka1(1, 2);
+angka1(1, 2, 3);
+const [ax, ...bx] = [1, 2, 4, 5];
+angka1(ax, ...bx);
+
+const x = [2, 3, 5];
+angka1(...x); // Spread mengeluarkan angka dari array menjadi 2,3,4 -> kemudian nilai-nilai ini akan dikirimkan ke function angka1
+/* ----- Contoh Kasus ----- */
+
+rumahMakan.mieRebus("sawi", "daun bawang", "sledri", "kobis", "telur"); // sawi // daun bawang sledri kobis telur
